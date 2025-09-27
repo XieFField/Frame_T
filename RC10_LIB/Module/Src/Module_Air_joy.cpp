ce31da7e9c7d8653e471bb26792498c8a201fd6e
zhenghuo669 <1017635694@qq.com>
2025-09-27 13:20:28 +0800

    更新遥控
    P

--- RC10_LIB/Module/Src/Module_Air_joy.cpp
+++ RC10_LIB/Module/Src/Module_Air_joy.cpp
@@ -10,7 +10,6 @@ volatile int cnt_ = 0;
 
 void AirJoy::data_update(uint16_t GPIO_Pin, uint16_t GPIO_EXTI_USED_PIN)
 {
-    
     if(GPIO_Pin != GPIO_EXTI_USED_PIN) 
         return;
     
@@ -31,34 +30,11 @@ void AirJoy::data_update(uint16_t GPIO_Pin, uint16_t GPIO_EXTI_USED_PIN)
         } 
         else if(ppm_time_delta >= PWM_MIN && ppm_time_delta <= PWM_MAX) // 单个PWM脉宽在1000-2000us
         {         
-            // 限幅滤波：突变 > 15% 丢弃整帧
-            uint16_t new_val = ppm_time_delta;
-            
-            // 对于第一个通道或未初始化的通道，直接接受
-            if(ppm_sample_cnt == 0 || last_valid[ppm_sample_cnt] == 0) 
-            {
-                PPM_buf[ppm_sample_cnt] = new_val;
-                last_valid[ppm_sample_cnt] = new_val;
-                ppm_sample_cnt++;
-            }
-            else
-            {
-                uint16_t old_val = last_valid[ppm_sample_cnt];
-                uint16_t threshold = (old_val * FILTER_THRESHOLD_PERCENT) / 100;
-                
-                if(abs((int)new_val - (int)old_val) > threshold)   
-                {
-                    ppm_ready = 0;
-                    ppm_sample_cnt = 0;
-                    return;  // 直接退出
-                }
-                
-                // 通过滤波，存入缓冲区
-                PPM_buf[ppm_sample_cnt] = new_val;
-                last_valid[ppm_sample_cnt] = new_val;
-                ppm_sample_cnt++;
-                cnt_ = ppm_sample_cnt;
-            }
+            // 直接存入缓冲区，不做任何滤波
+            PPM_buf[ppm_sample_cnt] = ppm_time_delta;
+            last_valid[ppm_sample_cnt] = ppm_time_delta;
+            ppm_sample_cnt++;
+            cnt_ = ppm_sample_cnt;
             
             // 边界检查
             if(ppm_sample_cnt >= MAX_CHANNELS)   
